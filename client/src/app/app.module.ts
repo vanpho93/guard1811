980ab0ee946afe12fa82cfa9e5dbb30fc63ee685
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-03 20:08:10 +0700

    services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -36,6 +37,7 @@ const routesConfig: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     RouterModule.forRoot(routesConfig),
   ],
   providers: [],
